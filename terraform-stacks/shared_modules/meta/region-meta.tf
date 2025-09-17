4db465b4e82c6eea2f17a90dd07e61ab739a1636
davidfos <david.d.foster@oracle.com>
2025-09-17 10:30:13 -0700

    fix region_metadata retrieval failures

--- terraform-stacks/shared_modules/meta/region-meta.tf
+++ terraform-stacks/shared_modules/meta/region-meta.tf
@@ -1,20 +1,19 @@
 # fetch the metadata of regionInfo from IMDS
 data "external" "instance_regionInfo" {
   program = ["bash", "-c", <<EOT
-set -e
-METADATA=$(curl -s -H "Authorization: Bearer Oracle" http://169.254.169.254/opc/v2/instance/regionInfo)
-# Simple check if output starts with '{' to roughly verify JSON format.
-# On non-OCI instances, the curl may return an error HTML page instead of JSON. In this case, the script with treat the region as non-restricted.
+METADATA=$(curl -s --connect-timeout 5 --max-time 10 -H "Authorization: Bearer Oracle" http://169.254.169.254/opc/v2/instance/regionInfo)
 if [[ "$METADATA" == \{* ]]; then
   echo "$METADATA"
 else
-  # Return an error string if the response is invalid or empty
-  echo "error"
+  echo "{}"
 fi
+exit 0
+
 EOT
   ]
 }
 
 locals {
-  region_metadata = try(data.external.instance_regionInfo.result, "error")
+  region_metadata    = try(data.external.instance_regionInfo.result, {})
+  metadata_available = lookup(local.region_metadata, "realmDomainComponent", "error") != "error"
 }
